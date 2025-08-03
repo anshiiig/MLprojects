de60f0907c73091e5d9c6f975e303cb305480d34
anshiiig <anshigupta1705@gmail.com>
2025-08-03 15:53:36 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass # this is decorator --> use to directly define our class variable
-class DataIngestionConfig:  # we have created this class so that we can store raw inputs
+class DataIngestionConfig:  # we have created this class so that we can store raw inputs/providing all imputs needed
     train_data_path: str=os.path.join('artifacts',"train.csv") #____ this is our input we are giving--> then data ingestion will save the train data in this particular path
     test_data_path: str=os.path.join('artifacts',"test.csv") #     |
     raw_data_path: str=os.path.join('artifacts',"data.csv") #  ____|---> this tell us where to store train ,test and raw data   
@@ -46,5 +49,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
         
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
